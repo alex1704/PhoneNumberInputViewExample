72c3c324c59577322b2a0342a17c1500909b1376
Alex Kostenko <kostenkoaleksander7@gmail.com>
2022-11-28 17:35:40 +0200

    Initial Commit

---
+++ PhoneNumberInputViewExample/PhoneNumberInputViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneNumberInputViewExampleApp.swift
+//  PhoneNumberInputViewExample
+//
+//  Created by Alex Kostenko on 28.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhoneNumberInputViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
